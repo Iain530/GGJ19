f917510ae6db2cb3d867ed1c743c45e038895034
Iain Cattermole <iain_c@live.co.uk>
2019-01-25 22:04:55 +0000

    big crab

---
+++ GGJ19/Assets/Scripts/CrabSize.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class CrabSize : MonoBehaviour {
+
+    public SpriteRenderer sr;
+    public Sprite[] sprites;
+
+    public int size = 0;
+
+    public int food = 0;
+    public int[] sizeIncreaseIntervals;
+
+    void Start() {
+        sr = GetComponent<SpriteRenderer>();
+        sr.sprite = sprites[size];
+    }
+
+    void IncreaseSize() {
+        size++;
+        sr.sprite = sprites[size];
+    }
+
+    void EatFood(int value) {
+        food += value;
+        if (food >= sizeIncreaseIntervals[size]) {
+            food = food - sizeIncreaseIntervals[size]
+            IncreaseSize();
+        }
+    }
+
+    int FoodToNextSize() {
+        return sizeIncreaseIntervals[size];
+    }
+}
