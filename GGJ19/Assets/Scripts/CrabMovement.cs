7540db9b9dfa19a573dfd83a4d2df8c43235464b
Iain Cattermole <iain_c@live.co.uk>
2019-01-25 21:36:20 +0000

    move the crab

--- GGJ19/Assets/Scripts/CrabMovement.cs
+++ GGJ19/Assets/Scripts/CrabMovement.cs
@@ -2,17 +2,36 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CrabMovement : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class CrabMovement : MonoBehaviour {
+
+    public int speed = 1;
+    public Rigidbody2D rb;
+
+    private Vector2 velocity;
+
+    void Start() {
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+        velocity.x = 0;
+        velocity.y = 0;
+
+        if (Input.GetKey(KeyCode.W)) {
+            velocity.y = 1;
+        } else if (Input.GetKey(KeyCode.S)) {
+            velocity.y = -1;
+        }
+
+        if (Input.GetKey(KeyCode.D)) {
+            velocity.x = 1;
+        } else if (Input.GetKey(KeyCode.A)) {
+            velocity.x = -1;
+        }
+
+        if (velocity.magnitude > 0) {
+            velocity = velocity / velocity.magnitude;
+        }
+        rb.velocity = velocity * speed;
     }
 }
