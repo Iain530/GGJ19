fe217dd6eac27381f56c07cf8799f236d94016b3
Iain Cattermole <iain_c@live.co.uk>
2019-01-26 23:41:44 +0000

    animated creb

---
+++ GGJ19/Assets/Sprites/beach_tiles.tsx
@@ -0,0 +1,171 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.1" name="Beach" tilewidth="512" tileheight="512" tilecount="36" columns="12">
+ <image source="sand_tiles.png" width="6144" height="1536"/>
+ <terraintypes>
+  <terrain name="BeachGrass" tile="13"/>
+  <terrain name="SandGrass" tile="13"/>
+  <terrain name="Water" tile="13"/>
+  <terrain name="SandWater" tile="13"/>
+ </terraintypes>
+ <tile id="0" terrain="0,0,0,3">
+  <objectgroup draworder="index">
+   <object id="1" x="228" y="282.667" width="281.333" height="224"/>
+  </objectgroup>
+ </tile>
+ <tile id="1" terrain="0,0,3,3">
+  <objectgroup draworder="index">
+   <object id="1" x="-6.66667" y="276" width="517.333" height="232"/>
+  </objectgroup>
+ </tile>
+ <tile id="2" terrain="0,0,3,0">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="264" width="230.667" height="245.333"/>
+  </objectgroup>
+ </tile>
+ <tile id="3" terrain="2,2,2,3">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="256" height="512"/>
+   <object id="2" x="256" y="0" width="256" height="256"/>
+   <object id="3" x="0" y="0" width="256" height="512"/>
+  </objectgroup>
+ </tile>
+ <tile id="4" terrain="2,2,3,3">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="1.33333" width="512" height="256"/>
+  </objectgroup>
+ </tile>
+ <tile id="5" terrain="2,2,3,2">
+  <objectgroup draworder="index">
+   <object id="1" x="1.33333" y="1.33333" width="512" height="234.667"/>
+   <object id="2" x="242.667" y="236" width="270.667" height="278.667"/>
+  </objectgroup>
+ </tile>
+ <tile id="6" terrain="3,3,3,2">
+  <objectgroup draworder="index">
+   <object id="1" x="-2.66667" y="-2.66667" width="229.333" height="513.333"/>
+   <object id="2" x="0" y="-4" width="508" height="200"/>
+  </objectgroup>
+ </tile>
+ <tile id="7" terrain="3,3,,">
+  <objectgroup draworder="index">
+   <object id="1" x="-1.33333" y="0" width="516" height="225.333"/>
+  </objectgroup>
+ </tile>
+ <tile id="8" terrain="3,3,2,3">
+  <objectgroup draworder="index">
+   <object id="1" x="2.66667" y="4" width="498.667" height="224"/>
+   <object id="2" x="309.333" y="188" width="196" height="326.667"/>
+  </objectgroup>
+ </tile>
+ <tile id="9" terrain="3,3,3,0">
+  <objectgroup draworder="index">
+   <object id="1" x="262.667" y="237.333" width="250.667" height="276"/>
+  </objectgroup>
+ </tile>
+ <tile id="10" terrain="3,3,0,0">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="288" width="508" height="221.333"/>
+  </objectgroup>
+ </tile>
+ <tile id="11" terrain="3,3,0,3">
+  <objectgroup draworder="index">
+   <object id="2" x="-6.66667" y="292" width="236" height="218.667"/>
+  </objectgroup>
+ </tile>
+ <tile id="12" terrain="0,3,0,3">
+  <objectgroup draworder="index">
+   <object id="1" x="269.333" y="2.66667" width="241.333" height="510.667"/>
+  </objectgroup>
+ </tile>
+ <tile id="13" terrain="3,3,3,3"/>
+ <tile id="14" terrain="3,0,3,0">
+  <objectgroup draworder="index">
+   <object id="1" x="8" y="2.66667" width="218.667" height="509.333"/>
+  </objectgroup>
+ </tile>
+ <tile id="15" terrain="2,3,2,3">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="256" height="512"/>
+  </objectgroup>
+ </tile>
+ <tile id="16" terrain="3,3,3,3"/>
+ <tile id="17" terrain="3,2,3,2">
+  <objectgroup draworder="index">
+   <object id="1" x="252" y="0" width="256" height="510.667"/>
+  </objectgroup>
+ </tile>
+ <tile id="18" terrain="3,,3,">
+  <objectgroup draworder="index">
+   <object id="1" x="-2.66667" y="4" width="220" height="505.333"/>
+  </objectgroup>
+ </tile>
+ <tile id="19" terrain="2,2,2,2"/>
+ <tile id="20" terrain=",3,,3">
+  <objectgroup draworder="index">
+   <object id="4" x="297.333" y="0" width="212" height="512"/>
+  </objectgroup>
+ </tile>
+ <tile id="21" terrain="3,0,3,0"/>
+ <tile id="22" terrain="0,0,0,0"/>
+ <tile id="23" terrain="0,3,0,3"/>
+ <tile id="24" terrain="0,3,0,0">
+  <objectgroup draworder="index">
+   <object id="1" x="276" y="8" width="234.667" height="230.667"/>
+  </objectgroup>
+ </tile>
+ <tile id="25" terrain="3,3,0,0">
+  <objectgroup draworder="index">
+   <object id="1" x="2.66667" y="-2.66667" width="505.333" height="238.667"/>
+  </objectgroup>
+ </tile>
+ <tile id="26" terrain="3,0,0,0">
+  <objectgroup draworder="index">
+   <object id="1" x="4" y="4" width="237.333" height="230.667"/>
+  </objectgroup>
+ </tile>
+ <tile id="27" terrain="2,3,2,2">
+  <objectgroup draworder="index">
+   <object id="1" x="-6.66667" y="0" width="273.333" height="504"/>
+   <object id="2" x="274.667" y="264" width="233.333" height="240"/>
+  </objectgroup>
+ </tile>
+ <tile id="28" terrain="3,3,2,2">
+  <objectgroup draworder="index">
+   <object id="1" x="-1.33333" y="248" width="512" height="258.667"/>
+  </objectgroup>
+ </tile>
+ <tile id="29" terrain="3,2,2,2">
+  <objectgroup draworder="index">
+   <object id="1" x="-1.33333" y="245.333" width="510.667" height="269.333"/>
+   <object id="2" x="244" y="-1.33333" width="264" height="245.333"/>
+  </objectgroup>
+ </tile>
+ <tile id="30" terrain="3,2,3,3">
+  <objectgroup draworder="index">
+   <object id="1" x="-4" y="5.33333" width="202.667" height="510.667"/>
+   <object id="2" x="201.333" y="289.333" width="302.667" height="218.667"/>
+  </objectgroup>
+ </tile>
+ <tile id="31" terrain=",,3,3">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="304" width="510.667" height="205.333"/>
+  </objectgroup>
+ </tile>
+ <tile id="32" terrain="2,3,3,3">
+  <objectgroup draworder="index">
+   <object id="2" x="1.33333" y="340" width="509.333" height="168"/>
+   <object id="3" x="281.333" y="2.66667" width="226.667" height="337.333"/>
+  </objectgroup>
+ </tile>
+ <tile id="33" terrain="3,0,3,3">
+  <objectgroup draworder="index">
+   <object id="1" x="281.333" y="-1.33333" width="228" height="225.333"/>
+  </objectgroup>
+ </tile>
+ <tile id="34" terrain="0,0,3,3"/>
+ <tile id="35" terrain="0,3,3,3">
+  <objectgroup draworder="index">
+   <object id="1" x="-1.33333" y="1.33333" width="254.667" height="269.333"/>
+  </objectgroup>
+ </tile>
+</tileset>
