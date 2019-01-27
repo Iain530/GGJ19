73db90639163a373642a737a37277c19c9d449ae
= <->
2019-01-27 16:03:14 +0000

    Poop Prefab

--- GGJ19/Assets/Scripts/ItemPickup.cs
+++ GGJ19/Assets/Scripts/ItemPickup.cs
@@ -4,24 +4,9 @@ using UnityEngine;
 
 public class ItemPickup : MonoBehaviour
 {
-
-    // Custom behaviour for certain pickups?
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
     private void OnTriggerEnter2D(Collider2D other)
     {
-
-        if (other.gameObject.CompareTag ("Player") && other.gameObject.GetComponent<CrabSize>().HasShell())
+        if (other.gameObject.CompareTag ("Player"))
         {
             other.gameObject.GetComponent<CrabSize>().EatFood(10);
             this.gameObject.SetActive (false);
