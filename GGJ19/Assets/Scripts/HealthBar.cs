5bc66d6092cc1d6d8a6f92cdba07b288e973ada6
= <->
2019-01-27 12:35:41 +0000

    [ Asset ] health fill sprite on HPBar prefab

--- GGJ19/Assets/Scripts/HealthBar.cs
+++ GGJ19/Assets/Scripts/HealthBar.cs
@@ -5,12 +5,16 @@ using UnityEngine;
 public class HealthBar : MonoBehaviour
 {
     private Transform bar;
+    // public float v = 0.5f;
     // Start is called before the first frame update
     void Start()
     {
          bar = transform.Find("Bar");
     }
 
+// void Update(){
+    // SetSize(v);
+// }
     public void SetSize(float sizeNormalized)
     {
         bar.localScale = new Vector3(sizeNormalized, 1f);
