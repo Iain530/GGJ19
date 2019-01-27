085bb705349c3018fc46c4d8aeb93b892cb3a5d8
Joshua Richardson <joshua@richardson.tech>
2019-01-27 12:16:38 +0000

    Enemies can hurt you

---
+++ GGJ19/Assets/Scripts/CrabHealth.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrabHealth : MonoBehaviour
+{
+    public int health = 100;
+
+    private HealthBar _healthBar;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _healthBar = GameObject.Find("HealthBar").GetComponent<HealthBar>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void UpdateHealth(int newHealth)
+    {
+        if (newHealth <= 0)
+        {
+            //die
+            return;
+        }
+        
+        health = newHealth;
+        _healthBar.SetSize( newHealth / 100f);
+
+
+    }
+}
