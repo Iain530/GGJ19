4b16557ec1adc74ce8b00ba9e5229ebddff27abb
= <->
2019-01-27 15:47:38 +0000

    Wind

---
+++ GGJ19/Assets/WindManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindManager : MonoBehaviour
+{
+    public GameObject pref;
+    public Vector3 offset;
+
+    void Start(){
+        InvokeRepeating("S",1,2);
+    }
+
+    void S(){
+
+        Vector3 pos = Camera.main.ViewportToWorldPoint(new Vector3(1,1,0));
+        
+        Instantiate(pref,pos+offset, pref.transform.rotation);
+    }
+
+    // Update is called once per frame
+}
